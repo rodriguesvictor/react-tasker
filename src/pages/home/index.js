12f0648ce13c63127371520d55755abebbbaf27e
Victor Rodrigues <Victordarkiin@gmail.com>
2022-12-08 18:17:30 -0300

    Add HomePage, Style

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import { Container } from "./styles";
+const Home = () => {
+  return (
+	<>
+	<Container>
+		<Header />
+	</Container>
+	</>
+  )
+}
+
+export default Home
---
+++ src/pages/home/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	max-width: 500px;
+	margin: 30px auto;
+	overflow: auto;
+	min-height: 300px;
+	border: 1px solid steelblue;
+	padding: 30px;
+	border-radius: 5px;
+`
