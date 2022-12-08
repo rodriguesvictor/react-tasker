1af65d392c6b4d62617a1da95fbb686c853bea3c
Victor Rodrigues <Victordarkiin@gmail.com>
2022-12-08 18:18:15 -0300

    Add Button component, style

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+import { ButtonContainer } from "./styles";
+const Button = ({text}) => {
+  return (
+	<ButtonContainer>{text}</ButtonContainer>
+  )
+}
+
+export default Button
---
+++ src/components/Button/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+	display: inline-block;
+	background: #000;
+	color: #fff;
+	border: none;
+	padding: 10px 20px;
+	margin: 5px;
+	border-radius: 5px;
+	cursor: pointer;
+	text-decoration: none;
+	font-size: 15px;
+	font-family: inherit;
+
+	&:focus {
+		outline: none;
+	}
+
+	&:active {
+		transform: scale(0.98);
+		color: green;
+	}
+`
