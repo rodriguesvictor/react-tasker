9fce7882ccd9c7969a3a29797b49b1bc2e31af27
Victor Rodrigues <Victordarkiin@gmail.com>
2022-12-10 18:09:07 -0300

    Add dynamic props on Header components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,22 @@
 import Button from "../Button";
 import { HeaderTitle, Wrapper } from "./styles";
+import PropTypes from "prop-types";
 
-const Header = () => {
-	const onClick = () => {
-		console.log("Click!");
-	}
+
+const Header = ({ title, onAdd, showAdd }) => {
+
 	return (
 		<Wrapper>
-			<HeaderTitle>Task Tracker</HeaderTitle>
-			<Button text="Add" onClick={onClick} />
+			<HeaderTitle>{title}</HeaderTitle>
+			<Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
 		</Wrapper>
 	)
 }
 
+Header.defaultProps = {
+	title: 'Task Tracker',
+}
+Header.propTypes = {
+	title: PropTypes.string.isRequired,
+}
 export default Header
