b139de7ff30c017721b7787b94753e6f3b0bb102
Victor Rodrigues <Victordarkiin@gmail.com>
2022-12-10 18:07:16 -0300

    Add proptypes of new props on button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,17 @@
 import { ButtonContainer } from "./styles";
 import PropTypes from "prop-types";
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, variant="primary", onClick, color }) => {
 
 	return (
-		<ButtonContainer onClick={onClick}>{text}</ButtonContainer>
+		<ButtonContainer onClick={onClick} color={color} variant={variant}>{text}</ButtonContainer>
 	)
 }
 
 Button.propTypes = {
 	text: PropTypes.string,
 	onClick: PropTypes.func,
+	variant: PropTypes.string,
+	color: PropTypes.string,
 }
 export default Button
