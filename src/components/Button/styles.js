5589015ea5b7ae47ea002af0b5616df3ac56217d
Victor Rodrigues <Victordarkiin@gmail.com>
2022-12-10 18:46:02 -0300

    Add hover effect

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -12,15 +12,28 @@ export const ButtonContainer = styled.button`
 	text-decoration: none;
 	font-size: 15px;
 	font-family: inherit;
+	
+	-webkit-transform: perspective(1px) translateZ(0);
+	transform: perspective(1px) translateZ(0);
+	box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+	overflow: hidden;
+	-webkit-transition-duration: 0.3s;
+	transition-duration: 0.3s;
+	-webkit-transition-property: background-color;
+	transition-property: background-color;
 
+
+	&:hover {
+		background-color: black;
+		transform: scale(0.98);
+	}
 
 	&:focus {
-		outline: none;
+		utline: none;
 	}
 
 	&:active {
 		transform: scale(0.98);
-		color: green;
 	}
 
 	${({ variant }) => variant !== "primary" && css`
